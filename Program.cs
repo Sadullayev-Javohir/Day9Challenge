ba9b6a6920ad1cdb259f39e2cb3e7e5810bb8261
Javohir <javohirsadullayev2024@gmail.com>
2025-12-03 13:55:39 +0500

    Abstractoin added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿abstract class University
+{
+  public void Teach()
+  {
+    Console.WriteLine("Teacher teach students");
+  }
+
+  public abstract void Examination();
+}
+
+class Pdp : University
+{
+  public override void Examination()
+  {
+    Console.WriteLine("Examination begin");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+        var pdp = new Pdp();
+        pdp.Examination();
+        pdp.Teach();
+  }
+}
